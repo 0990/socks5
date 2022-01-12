8269eea5ceb96e0f20a72a5ea13249fa4bf78cf3
0990 <xjlong@outlook.com>
2022-01-12 19:33:06 +0800

    chore:增加生成arm二进制的脚本配置

--- build.bat
+++ build.bat
@@ -1,8 +1,14 @@
 ::build
 SET GOOS=linux
+SET GOARCH=amd64
 go build -o bin/ss5 cmd/server/main.go
 ::go build -o bin/udp_test cmd/client/udp_test/main.go
 
 SET GOOS=windows
+SET GOARCH=amd64
 go build -o bin/ss5.exe cmd/server/main.go
 ::go build -o bin/udp_test.exe cmd/client/udp_test/main.go
+
+SET GOOS=linux
+SET GOARCH=arm64
+go build -o bin/ss5_arm cmd/server/main.go
